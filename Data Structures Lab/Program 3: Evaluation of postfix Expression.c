c7ebb997cf112990cd3e486ac83929692c4b59e7
Ashwin Bekal <97862377+ashwinbekal@users.noreply.github.com>
2023-02-19 01:25:44 +0530

    Create Program 3: Evaluation of postfix Expression.c

---
+++ Data Structures Lab/Program 3: Evaluation of postfix Expression.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int stack[50],top=-1;
+
+void push(int ele){
+    stack[++top]=ele;
+}
+
+void main(){
+    char postfix[50],ch;
+    int i=0,op1,op2;
+    printf("Enter a suffix operation with single digit operands and operators:\n");
+    scanf("%s",postfix);
+    while((ch=postfix[i++])!='\0'){
+        if(isalpha(ch)){
+            printf("Invalid Expression");
+            return;
+        }
+        else if(isdigit(ch)){
+            push(ch-48);
+        }
+        else{
+            op2=stack[top--];
+            if(top<=-1){
+                printf("Invalid Expression\n");
+                return;
+            }
+            op1=stack[top--];
+            switch(ch){
+                case '+':push(op1+op2);break;
+                case '-':push(op1-op2);break;
+                case '*':push(op1*op2);break;
+                case '/':push(op1/op2);break;
+                case '%':push(op1%op2);break;
+                case '^':push(op1^op2);break;
+                default:printf("Invalid Operator\n");
+                return;
+            }
+        }
+    }
+    if(top!=0) printf("Invalid Expression\n");
+    else printf("Result= %d\n",stack[top]);
+}
